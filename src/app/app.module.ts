ac14b35d9119f1fcbc634a76fcb0a557228789be
DannyKaschub <dannykashub98@gmail.com>
2023-01-28 11:23:54 +0100

    eibau der skills section und about me responsive machen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { FooterComponent } from './footer/footer.component';
+import { StartComponent } from './start/start.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    FooterComponent,
+    StartComponent,
+    SkillsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/skills/skills.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.sass']
+})
+export class SkillsComponent {
+
+  techs = [
+    { name: "Angular", imageLink: "../assets/img/skillIcons/Angular.png" },
+    { name: "TypeScript", imageLink: "../assets/img/skillIcons/typeScript.png" },
+    { name: "JavaScript", imageLink: "../assets/img/skillIcons/Javascript.png" },
+    { name: "HTML", imageLink: "../assets/img/skillIcons/html.png" },
+    { name: "Firebase", imageLink: "../assets/img/skillIcons/Firebase.png" },
+    { name: "Git", imageLink: "../assets/img/skillIcons/git.png" },
+    { name: "CSS/SASS", imageLink: "../assets/img/skillIcons/css.png" },
+    { name: "Rest-Api", imageLink: "../assets/img/skillIcons/API.png" },
+    { name: "Scrum", imageLink: "../assets/img/skillIcons/scrum.png" },
+    { name: "Material Design", imageLink: "../assets/img/skillIcons/MaterialDesign.png" }
+  ];
+
+}
